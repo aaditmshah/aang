1db14e52adb0d44083af40dde7518bb40d83a3ee
Aadit M Shah <aaditmshah@aadit.codes>
2024-01-31 18:51:50 +0530

    ✨ Move all fast-check arbitraries to separate file
    Our custom fast-check arbitraries will be used in multiple test files.
    Hence, it makes sense to move them all to one place so that we can reuse
    them.

---
+++ tests/arbitraries.ts
@@ -0,0 +1,38 @@
+import fc from "fast-check";
+
+import type { Expression, NonFunction, Thunk } from "../src/expression.js";
+import { isNonFunction } from "../src/expression.js";
+import type { Option } from "../src/option.js";
+import { None, Some } from "../src/option.js";
+import type { Result } from "../src/result.js";
+import { Failure, Success } from "../src/result.js";
+
+export const thunk = <A>(a: fc.Arbitrary<A>): fc.Arbitrary<Thunk<A>> =>
+  a.map((value) => () => value);
+
+export const nonFunction = <A>(
+  a: fc.Arbitrary<A>,
+): fc.Arbitrary<NonFunction<A>> => a.filter(isNonFunction);
+
+export const expression = <A>(
+  a: fc.Arbitrary<A>,
+): fc.Arbitrary<Expression<A>> => fc.oneof(thunk(a), nonFunction(a));
+
+export const some = <A>(a: fc.Arbitrary<A>): fc.Arbitrary<Some<A>> =>
+  a.map((value) => new Some(value));
+
+export const none: fc.Arbitrary<None> = fc.constant(None.instance);
+
+export const option = <A>(a: fc.Arbitrary<A>): fc.Arbitrary<Option<A>> =>
+  fc.oneof(some(a), none);
+
+export const success = <A>(a: fc.Arbitrary<A>): fc.Arbitrary<Success<A>> =>
+  a.map((value) => new Success(value));
+
+export const failure = <E>(b: fc.Arbitrary<E>): fc.Arbitrary<Failure<E>> =>
+  b.map((error) => new Failure(error));
+
+export const result = <E, A>(
+  a: fc.Arbitrary<A>,
+  b: fc.Arbitrary<E>,
+): fc.Arbitrary<Result<E, A>> => fc.oneof(success(a), failure(b));
