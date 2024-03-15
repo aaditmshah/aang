47811135fefb83760e068e6382852dc62191e51d
Aadit M Shah <aaditmshah@aadit.codes>
2024-03-15 17:00:14 +0530

    💥 Return a union type from the option `or` method
    I returned a union type from the option `or` method because it matches
    the categorical definition of monoidal functors. In addition, it's makes
    the API easier to use. This is a breaking change because now users will
    have to specify two type parameters if they're not relying on inference.

--- src/option.ts
+++ src/option.ts
@@ -41,7 +41,7 @@ abstract class OptionTrait
     return this.isSome && that.isSome ? this : None.instance;
   }
 
-  public or<A>(this: Option<A>, that: Option<A>): Option<A> {
+  public or<A, B>(this: Option<A>, that: Option<B>): Option<A | B> {
     return this.isSome ? this : that;
   }
 
