9c71117494c3f832bb8990b026151861ce8796d5
Aadit M Shah <aaditmshah@aadit.codes>
2024-04-01 11:00:15 +0530

    ✨ Add `uncurry2`, `uncurry3`, & `uncurry4` functions
    The `uncurry2`, `uncurry3`, and `uncurry4` functions are meant to be
    used in conjunction with the applicative interface of data types. For
    example, you can write `u.and(v).and(w).and(x).map(uncurry4(f))`. This
    is equivalent to `f <$> u <*> v <*> w <*> x` in Haskell. We only provide
    `uncurry` functions up to 4 parameters. If the programmer needs more
    parameters, then they are expected to define their own `uncurry`
    functions. The expectation is that `uncurry2`, `uncurry3`, and
    `uncurry4` should be able to handle 99% of use cases.

--- src/miscellaneous.ts
+++ src/miscellaneous.ts
@@ -1,1 +1,20 @@
+import type { Pair } from "./pair.js";
+
 export const id = <A>(value: A): A => value;
+
+export const uncurry2 =
+  <A, B, C>(morphism: (a: A, b: B) => C) =>
+  ({ fst: a, snd: b }: Pair<A, B>): C =>
+    morphism(a, b);
+
+export const uncurry3 =
+  <A, B, C, D>(morphism: (a: A, b: B, c: C) => D) =>
+  ({ fst: { fst: a, snd: b }, snd: c }: Pair<Pair<A, B>, C>): D =>
+    morphism(a, b, c);
+
+export const uncurry4 =
+  // prettier-ignore
+  <A, B, C, D, E>(morphism: (a: A, b: B, c: C, d: D) => E) => ({
+    fst: { fst: { fst: a, snd: b }, snd: c }, snd: d
+  }: Pair<Pair<Pair<A, B>, C>, D>): E =>
+    morphism(a, b, c, d);
---
+++ tests/miscellaneous.test.ts
@@ -0,0 +1,97 @@
+import { describe, expect, it } from "@jest/globals";
+import fc from "fast-check";
+
+import type { Option } from "../src/option.js";
+import { uncurry2, uncurry3, uncurry4 } from "../src/miscellaneous.js";
+
+import { option } from "./arbitraries.js";
+
+const uncurry2Equivalence = <A, B, C>(
+  u: Option<A>,
+  v: Option<B>,
+  f: (a: A, b: B) => C,
+): void => {
+  expect(u.and(v).map(uncurry2(f))).toStrictEqual(
+    u.and(v).map((x) => x.fold(f)),
+  );
+};
+
+const uncurry3Equivalence = <A, B, C, D>(
+  u: Option<A>,
+  v: Option<B>,
+  w: Option<C>,
+  f: (a: A, b: B, c: C) => D,
+): void => {
+  expect(u.and(v).and(w).map(uncurry3(f))).toStrictEqual(
+    u
+      .and(v)
+      .and(w)
+      .map((x) => x.fold((y, c) => uncurry2((a: A, b: B): D => f(a, b, c))(y))),
+  );
+};
+
+const uncurry4Equivalence = <A, B, C, D, E>(
+  u: Option<A>,
+  v: Option<B>,
+  w: Option<C>,
+  x: Option<D>,
+  f: (a: A, b: B, c: C, d: D) => E,
+): void => {
+  expect(u.and(v).and(w).and(x).map(uncurry4(f))).toStrictEqual(
+    u
+      .and(v)
+      .and(w)
+      .and(x)
+      .map((y) =>
+        y.fold((z, d) => uncurry3((a: A, b: B, c: C): E => f(a, b, c, d))(z)),
+      ),
+  );
+};
+
+describe("uncurry2", () => {
+  it("should agree with fold", () => {
+    expect.assertions(100);
+
+    fc.assert(
+      fc.property(
+        option(fc.anything()),
+        option(fc.anything()),
+        fc.func(fc.anything()),
+        uncurry2Equivalence,
+      ),
+    );
+  });
+});
+
+describe("uncurry3", () => {
+  it("should agree with uncurry2", () => {
+    expect.assertions(100);
+
+    fc.assert(
+      fc.property(
+        option(fc.anything()),
+        option(fc.anything()),
+        option(fc.anything()),
+        fc.func(fc.anything()),
+        uncurry3Equivalence,
+      ),
+    );
+  });
+});
+
+describe("uncurry4", () => {
+  it("should agree with uncurry3", () => {
+    expect.assertions(100);
+
+    fc.assert(
+      fc.property(
+        option(fc.anything()),
+        option(fc.anything()),
+        option(fc.anything()),
+        option(fc.anything()),
+        fc.func(fc.anything()),
+        uncurry4Equivalence,
+      ),
+    );
+  });
+});
